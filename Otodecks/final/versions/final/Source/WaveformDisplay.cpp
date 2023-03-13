4d8daa9d926556441478417512f9a4ef4f164ff1
Ahmad Khan <ahmadnawazkhan1@outlook.com>
2023-03-13 13:07:00 +0500

    finish docs

---
+++ Otodecks/final/versions/final/Source/WaveformDisplay.cpp
@@ -0,0 +1,83 @@
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "WaveformDisplay.h"
+
+//==============================================================================
+WaveformDisplay::WaveformDisplay(juce::AudioFormatManager &formatManagerToUse,
+                                 juce::AudioThumbnailCache &cacheToUse,
+                                 DJAudioPlayer *_player)
+        : player(_player),
+          audioThumb(1000,
+                     formatManagerToUse,
+                     cacheToUse),
+          fileLoaded(false),
+          position(0) {
+    // In your constructor, you should add any child components, and
+    // initialise any special settings that your component needs.
+
+    audioThumb.addChangeListener(this);
+}
+
+WaveformDisplay::~WaveformDisplay() {
+}
+
+void WaveformDisplay::paint(juce::Graphics &g) {
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));   // clear the background
+
+    g.drawRect(getLocalBounds(), 1);   // draw an outline around the component
+
+    if (fileLoaded) {
+        g.setColour(juce::Colour::fromRGB(115, 163, 248));
+        audioThumb.drawChannel(g,
+                               getLocalBounds(),
+                               0,
+                               audioThumb.getTotalLength(),
+                               0,
+                               1.0f
+        );
+        g.setColour(juce::Colours::red);
+        g.fillRect(position * getWidth(), 0, 2, getHeight());
+    } else {
+        g.setColour(juce::Colours::white);
+        g.setFont(20.0f);
+        g.drawText("File not loaded...", getLocalBounds(),
+                   juce::Justification::centred, true);
+    }
+}
+
+void WaveformDisplay::resized() {}
+
+void WaveformDisplay::loadURL(juce::URL audioURL) {
+    audioThumb.clear();
+    fileLoaded = audioThumb.setSource(new juce::URLInputSource(audioURL));
+    if (fileLoaded) {
+        std::cout << "wfd: loaded! " << std::endl;
+        repaint();
+    } else {
+        std::cout << "wfd: not loaded! " << std::endl;
+    }
+
+}
+
+void WaveformDisplay::changeListenerCallback(juce::ChangeBroadcaster *source) {
+    std::cout << "wfd: change received! " << std::endl;
+    repaint();
+}
+
+void WaveformDisplay::setPositionRelative(double pos) {
+    if (pos != position) {
+        position = pos;
+        repaint();
+    }
+}
+
+void WaveformDisplay::mouseDown(const juce::MouseEvent &event) {
+    // Update position based on mouse click position
+    auto pos = (double) event.x / getWidth();
+    player->setPositionRelative(pos);
+    setPositionRelative(pos);
+}
+
+
+
+
---
+++ Otodecks/final/versions/final/Source/domain/Track.cpp
@@ -0,0 +1,110 @@
+
+#include "Track.h"
+
+Track::Track(
+        std::string title,
+        std::string album,
+        float duration,
+        juce::URL fileURL) :
+        title(title),
+        album(album),
+        duration(
+                duration),
+        fileURL(fileURL) {
+}
+
+std::string Track::getTitle() {
+    return title;
+}
+
+std::string Track::setTitle(const std::string _title) {
+    title = _title;
+    return title;
+}
+
+std::string Track::getAlbum() {
+    return album;
+}
+
+std::string Track::setAlbum(const std::string _album) {
+    album = _album;
+    return album;
+}
+
+float Track::getDuration() {
+    return duration;
+}
+
+std::string Track::getParsedDuration() {
+    int minutes;
+    minutes = (int) duration / 60;
+    int seconds = (int) duration % 60;
+    std::string parsedDuration = std::to_string(minutes) + ":" + (seconds < 10 ? "0" : "") + std::to_string(seconds);
+    return parsedDuration;
+}
+
+juce::URL Track::getFileURL() {
+    return fileURL;
+}
+
+std::string Track::getFileURLStr() {
+    return fileURL.toString(true).toStdString();
+}
+
+Track Track::parseFrom(const juce::File &file) {
+
+    juce::AudioFormatManager formatManager;
+    formatManager.registerBasicFormats();
+
+    std::string title = file.getFileNameWithoutExtension().toStdString();
+    std::string album = "Unknown";
+    juce::URL fileURL = juce::URL{file};
+    float duration = 0.0f;
+
+    // get the track's duration
+    std::unique_ptr<juce::AudioFormatReader> reader(formatManager.createReaderFor(file));
+    if (reader != nullptr) {
+        // log the track's metadata
+        juce::StringPairArray metadata = reader->metadataValues;
+        // loop through and log
+        for (auto &key : metadata.getAllKeys()) {
+            DBG("Metadata: " << key.toStdString() << " = " << metadata[key].toStdString());
+        }
+
+        // get album if it exists in the metadata
+        if (metadata.getAllKeys().contains(juce::WavAudioFormat::riffInfoProductName)) {
+            album = metadata[juce::WavAudioFormat::riffInfoProductName].toStdString();
+        }
+        else if (metadata.getAllKeys().contains("Album")) {
+            album = metadata["Album"].toStdString();
+        }
+        else if (metadata.getAllKeys().contains(juce::OggVorbisAudioFormat::id3album)) {
+            album = metadata[juce::OggVorbisAudioFormat::id3album].toStdString();
+        }
+
+
+        const double durationInSeconds = reader->lengthInSamples / reader->sampleRate;
+        duration = durationInSeconds;
+    }
+
+    return Track(title, album, duration, fileURL);
+}
+
+Track Track::parseFrom(const juce::var &trackVar) {
+    std::string title = trackVar["title"].toString().toStdString();
+    std::string album = trackVar["album"].toString().toStdString();
+    float duration = trackVar["duration"];
+    juce::URL fileURL = juce::URL{trackVar["fileURL"].toString()};
+
+    return Track(title, album, duration, fileURL);
+}
+
+juce::var Track::toVar(Track track) {
+    auto trackVar = new juce::DynamicObject();
+    trackVar->setProperty("title", juce::var(track.getTitle()));
+    trackVar->setProperty("album", juce::var(track.getAlbum()));
+    trackVar->setProperty("duration", juce::var(track.getDuration()));
+    trackVar->setProperty("fileURL", juce::var(track.getFileURLStr()));
+
+    return trackVar;
+}
